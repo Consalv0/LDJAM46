285b227ed1c08990b4c798dfb2e4c9f715f9c549
DiegoClorioTW <diego.clorio@tagwizz.com.mx>
2020-04-18 00:04:05 -0500

    Player script y Platform Script

---
+++ Assets/Clorio/Scripts/Platform.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    public bool isIce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (collision.gameObject.GetComponent<Rigidbody>().velocity.y > 0)
+            {
+                Destroy(gameObject.transform.parent.gameObject);
+            }
+        }
+    }
+
+    
+}
---
+++ Assets/Clorio/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float jumpVelocity;
+    public float gravityScale = 1.0f;
+    public static float globalGravity = -9.81f;
+    bool jumpRequest;
+    float movement;
+    public float speed; 
+    
+    void Start()
+    {
+        jumpRequest = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpRequest = true;
+        }
+
+        movement = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().MovePosition(transform.position + new Vector3(movement * speed * Time.deltaTime, 0, 0));
+
+        if (jumpRequest)
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * jumpVelocity, ForceMode.Impulse);
+            jumpRequest = false;
+        }
+
+        Vector3 gravity = globalGravity * gravityScale * Vector3.up;
+        GetComponent<Rigidbody>().AddForce(gravity, ForceMode.Acceleration);
+    }
+}
