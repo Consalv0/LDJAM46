c9f5189578b19bf0585757419838fb2af924c8d9
Consalvo <gonzaloceciliano@gmail.com>
2020-04-19 13:41:54 -0500

    Test scene working

---
+++ Assets/Gonzalo/Scripts/PlatformObject.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewPlatform", menuName = "Game/Platform")]
+public class PlatformObject : ScriptableObject
+{
+    public GameObject visuals;
+    public Vector2 dimensions;
+}
